c86f92c9839481043611b655affaf0f7337008d2
bbarrett11 <bjb2us@virginia.edu>
2018-09-26 22:10:42 -0400

    Problem 43 brute force

---
+++ src/problem/solutions/Problem43.java
@@ -0,0 +1,38 @@
+package problem.solutions;
+
+public class Problem43 {
+	public static void main(String[] args)
+	{
+		long sum = 0;
+		for(long i = 1000000000; i < 9999999999l;i++)
+		{
+			char[] nums = (i+"").toCharArray();
+			int[] count = new int[10];
+			for(char c : nums)
+			{
+				count[c-48]++;
+			}
+			boolean isPand=true;
+			for(int h : count)
+			{
+				if(h>1)
+					isPand = false;
+			}
+			System.out.println(i);
+			if(!isPand)
+				continue;
+			if(Long.valueOf(""+nums[1]+nums[2]+nums[3])%2==0 &&
+					Long.valueOf(""+nums[2]+nums[3]+nums[4])%3==0 &&
+					Long.valueOf(""+nums[3]+nums[4]+nums[5])%5==0 &&
+					Long.valueOf(""+nums[4]+nums[5]+nums[6])%7==0 &&
+					Long.valueOf(""+nums[5]+nums[6]+nums[7])%11==0 &&
+					Long.valueOf(""+nums[6]+nums[7]+nums[8])%13==0 &&
+					Long.valueOf(""+nums[7]+nums[8]+nums[9])%17==0)
+			{
+				sum+=i;
+			}
+		}
+		System.out.println(sum);
+	}
+	
+}
