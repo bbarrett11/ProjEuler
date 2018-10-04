72ec9e2880eb27ad33b81e666aeabcde6a3d6f41
bbarrett11 <bjb2us@virginia.edu>
2018-10-04 00:47:54 -0400

    Problem 44 solution

--- src/problem/solutions/Problem44.java
+++ src/problem/solutions/Problem44.java
@@ -1,27 +1,32 @@
 package problem.solutions;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem44 {
 	public static void main(String[] args)
 	{
-		LinkedList<Integer> l = new LinkedList<Integer>();
+		long min = 99999999999999l;
 		int numElements = 50000;
-		for(int i = 1; i <= numElements;i++)
+		long[] l = new long[numElements];
+		HashMap<Long,Boolean> m = new HashMap<Long,Boolean>();
+		for(int i = 1; i < numElements;i++)
 		{
-			l.add(i*(3*i-1)/2);
+			l[i] = (i*(3*i-1)/2);
+			m.put((long)i*(3*i-1)/2,true);
 		}
 		//System.out.println(l.toString());
-		for(int i = 0; i < l.size(); i++)
+		for(int i = 1; i < l.length; i++)
 		{
-			for(int h = 0; h < l.size(); h++)
+			for(int h = 1; h < l.length; h++)
 			{
-				if(i==h)
+				if(i==h || Math.abs(l[i]-l[h]) >= min)
 					continue;
-				if(l.contains(Math.abs(l.get(i)-l.get(h))) && l.contains(Math.abs(l.get(i)+l.get(h))))
+				if(m.containsKey(Math.abs(l[i]-l[h])) && m.containsKey(Math.abs(l[i]+l[h])) && m.get(Math.abs(l[i]-l[h])) && m.get(Math.abs(l[i]+l[h])))
 						{
-					System.out.println(Math.abs(l.get(i)-l.get(h)));
+						min = Math.min(min, Math.abs(l[i]-l[h]));
+						System.out.println(min);
 						}
 			}
 		}
